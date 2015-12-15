a667e0edf5ae4c2bd6ad606a75b985eae1d68d4e
xuxiangyang <xxy@creatingev.com>
2015-12-15 22:14:29 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('babel-polyfill') // ECMA6仿真
+//require("bootstrap-webpack"); //引入了bootstrap
+import React from "react"
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello world</h1>,
+    document.getElementById("root")
+);
+
