e5314e8b3074ce2025a488af60579040f39d430a
here0717 <here0717@gmail.com>
2015-12-30 12:35:28 +0800

    update app reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerStateReducer } from 'redux-router';
 
 import count from "./count"
+import post from "./post.js"
 
 const App = combineReducers({
   count: count,
+  post: post,
   router: routerStateReducer
 });
 
