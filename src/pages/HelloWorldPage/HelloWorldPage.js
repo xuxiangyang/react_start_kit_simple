cf544630b7bb164ec73bf676d6620b14720471a3
here0717 <here0717@gmail.com>
2015-12-30 12:35:42 +0800

    update page

--- src/pages/HelloWorldPage/HelloWorldPage.js
+++ src/pages/HelloWorldPage/HelloWorldPage.js
@@ -7,12 +7,14 @@ import { Link } from 'react-router';
 
 import Slogan from "components/Slogan/Slogan"
 import Footer from "components/Footer/Footer"
+import Posts from "components/Posts/Posts"
 import { PROVIDER } from "config"
 import { increase } from "action_creators/count"
 
 //这个state是全局store中的state。用它转换为props。一层一层的传递下去
 function mapStateToProps(state) {
   return {
+    post: state.post,
     count: state.count
   };
 }
@@ -28,6 +30,7 @@ class HelloWorldPage extends Component {
         <Slogan title="Hello World" content={`Welcome to React World ${this.props.count}`}/>
         <Link to="/hi"><h2>Hi</h2></Link>
         <a onClick={this.increaseCount.bind(this)}><h3>Increase number</h3></a>
+        <Posts dispatch={this.props.dispatch} post={this.props.post}/>
         <Footer content={`provide by ${PROVIDER}`}/>
       </div>
     )
